ac3b3974e88c749df2de9393c5e8b8add016d33b
Gosia Swider <gosia.mgs@gmail.com>
2015-05-05 19:19:18 +0000

    create Project model and all views for it(new, edit, show, index)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,51 @@
 class ProjectsController < ApplicationController
+  def index
+    @project = Project.all
+  end
+  
+  def show
+    @project = Project.find(params[:id])
+  end
+  def tresc
+    @project = Project.find(params[:id])
+  end
+  
   def new
+    @project = Project.new
+  end
+   def edit
+     @project = Project.find(params[:id])
   end
   
   def create
-    render plain: params[:article].inspect
-  end  
+    @project = Project.new(project_params)
+ 
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @project = Project.find(params[:id])
+ 
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
+   end
+ 
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+ 
+    redirect_to articles_path
+  end
+  
+  private
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ActiveRecord::Base
+  validates :name, presence: true,
+                    length: { minimum: 5 }
 end
