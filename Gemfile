de6eb15827b017604ce2b4dc1664605108d6fc87
Gosia Swider <gosia.mgs@gmail.com>
2015-05-10 15:31:53 +0000

    insall gem rspec and factoryGirl and create tests for User

--- Gemfile
+++ Gemfile
@@ -42,6 +42,10 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'rspec'
+  gem 'rspec-rails','~> 3.0'
+  gem 'shoulda-matchers'
+  gem 'factory_girl_rails'
   
   gem "better_errors"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  def full_name
+    [first_name, last_name].join(' ')
+  end
+  
+  def initials
+    first_name[0] + last_name[0]
+  end
 end
