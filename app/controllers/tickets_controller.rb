d5f85ad6838728ab19633ab1d83d2d8807bd20ab
Gosia Swider <gosia.mgs@gmail.com>
2015-05-13 17:56:17 +0000

    add index and new to ticket's views

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,39 @@
 class TicketsController < ApplicationController
+  
+  def index
+     @project = Project.find(params[:project_id])
+     @tickets = Project.find(params[:project_id]).tickets
+  end
+  
+  def show
+    @project = Project.find(params[:project_id])
+    @ticket = @project.tickets.find(params[:id])
+  end
+  
+  def new
+     @project = Project.find(params[:project_id])
+  end
+  
+  def edit
+    @project = Project.find(params[:project_id])
+    @ticket = @project.tickets.find(params[:id])
+  end
+  
+  def create
+    @project = Project.find(params[:project_id])
+    @ticket = @project.tickets.create(ticket_params)
+    redirect_to project_tickets_path(@project, @ticket)
+  end
+  
+  def destroy
+    @project = Project.find(params[:project_id])
+    @ticket = @project.tickets.find(params[:id])
+    @ticket.destroy
+    redirect_to project_tickets_path(@project, @ticket)
+  end
+  
+  private
+  def ticket_params
+     params.require(:ticket).permit(:name, :description, :priority, :difficulty, :status)
+    end
 end
