32fd45e98b547387709cce06a441bbfadff8ce53
Gosia Swider <gosia.mgs@gmail.com>
2015-05-19 12:22:25 +0000

    add project test

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,8 @@ class Ability
            can :manage, :all
          else
            can :read, :all
+           cannot :update, Project
+            cannot :update, Ticket
        end
     #
     # The first argument to `can` is the action you are giving the user
