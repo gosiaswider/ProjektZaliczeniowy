1dde811709e9dd13f1a3f07e59283cd4e3bd669f
Gosia Swider <gosia.mgs@gmail.com>
2015-05-19 14:52:10 +0000

    andd some new tests to ticket_spec

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -6,23 +6,45 @@ describe Ticket do
   it { should validate_presence_of :description }
   
   describe 'Bug' do
-    it 'returns Bug priority of ticket' do
+    it 'returns Bug difficulty of ticket' do
       ticket = Ticket.new(name: 'ticket', description: 'description', priority: 'Normal' , status:'Open' , difficulty: 'Bug')
       expect(ticket.difficulty).to eq 'Bug'
     end
   end
   
   describe 'Support' do
-    it 'returns support priority of ticket' do
+    it 'returns support difficulty of ticket' do
       ticket = Ticket.new(name: 'ticket', description: 'description', priority: 'Normal' , status:'Open' , difficulty: 'Support')
       expect(ticket.difficulty).to eq 'Support'
     end
   end
   
   describe 'Improvement' do
-    it 'returns Improvement priority of ticket' do
+    it 'returns Improvement difficulty of ticket' do
       ticket = Ticket.new(name: 'ticket', description: 'description', priority: 'Normal' , status:'Open' , difficulty: 'Improvement')
       expect(ticket.difficulty).to eq 'Improvement'
+    end
+  end
+  
+  
+  describe 'Low' do
+    it 'returns Low priority of ticket' do
+      ticket = Ticket.new(name: 'ticket', description: 'description', priority: 'Low' , status:'Open' , difficulty: 'Bug')
+      expect(ticket.priority).to eq 'Low'
+    end
+  end
+  
+  describe 'Normal' do
+    it 'returns normal priority of ticket' do
+      ticket = Ticket.new(name: 'ticket', description: 'description', priority: 'Normal' , status:'Open' , difficulty: 'Support')
+      expect(ticket.priority).to eq 'Normal'
+    end
+  end
+  
+  describe 'High' do
+    it 'returns high priority of ticket' do
+      ticket = Ticket.new(name: 'ticket', description: 'description', priority: 'High' , status:'Open' , difficulty: 'Improvement')
+      expect(ticket.priority).to eq 'High'
     end
   end
   
